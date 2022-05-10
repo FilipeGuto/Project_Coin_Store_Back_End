f3f4e9aa59f910ad1de5b82febc6f14ce84b3d5f
FilipeGuto <filipeguto95@gmail.com>
2022-05-09 22:49:05 -0300

    FIX: Arrumando conexão com banco

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,5 +2,5 @@ module.exports = (err, _req, res, _next) => {
   if (err.status) {
     return res.status(err.status).json({ message: err.message });
   }
-  return res.status(500).json({ message: 'Internal Server Error' });
+  return res.status(500).json({ message: 'Internal Server Error'});
 };
--- src/models/connection.js
+++ src/models/connection.js
@@ -1,15 +1,23 @@
-const mongodb = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL;
-const DB_NAME = 'Voll_Store';
+const MONGO_URL = process.env.MONGO_URL;
 
-module.exports = () => mongodb.connect(MONGO_DB_URL, {
+const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-.then((connection) => connection.db(DB_NAME))
-.catch((err) => {
-  console.log(err);
-  process.exit(1);
-});
+}
+
+let db = null;
+
+const connection = () => {
+  return db ?
+    Promise.resolve(db) :
+    MongoClient.connect(MONGO_URL, OPTIONS)
+      .then((conn) => {
+        db = conn.db('Voll_Store');
+        return db;
+      });
+};
+
+module.exports = connection;
