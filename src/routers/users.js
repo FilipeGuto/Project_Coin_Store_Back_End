bc3218fada3a3c668ea61ab6fc6b43344041cf69
FilipeGuto <filipeguto95@gmail.com>
2022-05-15 19:19:34 -0300

    FIX: alterando rota para requição correta

--- src/routers/users.js
+++ src/routers/users.js
@@ -15,6 +15,6 @@ routes.post('/users', controllerCreateUser);
 routes.post('/login', controllerLogin);
 routes.get('/users', auth, controllerFindUsers);
 routes.put('/users', auth, controllerUpdateUserCoin);
-routes.delete('/products/:id', auth, controllerDeleteUser);
+routes.delete('/users/:id', auth, controllerDeleteUser);
 
 module.exports = routes;
