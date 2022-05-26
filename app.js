f224501a7a139f0321ddd75180dc4cd83361320b
FilipeGuto <filipeguto95@gmail.com>
2022-05-26 20:39:38 -0300

    FIX: retirando codigos não usavel

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const routeProducts = require('./src/routers/products');
 const routeUsers = require('./src/routers/users');
 const errorMiddlewares = require('./src/middlewares/errorHandler');
 
 const app = express();
-app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(cors());
 app.use(express.json())
 
